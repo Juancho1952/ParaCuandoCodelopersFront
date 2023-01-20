5f62bb2c5de9dc8c9d7ea7520d7b9f48bcee42c7
Juancho1952 <juancho1952@gmail.com>
2023-01-20 17:46:19 -0500

    feat: change children

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <div>
-      <Layout> </Layout>
+      <Layout />
       <Header />
       <Footer />
     </div>
