82c4bf701954f9900f8ef26cb1f7f203126d85cf
Juancho1952 <juancho1952@gmail.com>
2023-01-27 18:31:34 -0500

    feat: add styles at button

--- pages/Signup.js
+++ pages/Signup.js
@@ -7,29 +7,29 @@ const Signup = () => {
   return (
     <>
       <div className="flex">
-        <div className='h-screen w-full bg-[url("../Assets/SingupImg/bg-Signup.png")] flex justify-center items-center content-center bg-cover bg-center bg-no-repeat'>
+        <div className='hidden sm:bg-[url("../Assets/SingupImg/bg-Signup.png")] sm:flex justify-center items-center content-center bg-cover bg-transparent bg-no-repeat h-screen w-full'>
           <Image
-            className="w-2/4"
+            className="sm:w-2/4"
             src={TextSignup}
             alt="marcas, artistas y torneos"
           />
           <Image className="w-1/4" src={TextSignupbg} alt="queretaro" />
         </div>
-        <div className="w-full pt-[50px] flex flex-col">
+        <div className="pt-9 w-full flex flex-col justify-center">
           <div className="w-full">
             <Image className="mx-auto" src={Paracuando} alt="Paracuandotext" />
           </div>
-          <div className="pl-10">
+          <div className="px-5">
             <h1>
               <strong>Sign up</strong>
             </h1>
             <p>Login with the data you entered during your registration</p>
-            <form className="pt-6 w-[85%]">
+            <form className="sm:flex flex-col gap-6 pt-6 w-[95%]">
               <div>
                 <div className="flex flex-col">
                   <label htmlFor="email">Email</label>
                   <input
-                    className="border-2 rounded h-9"
+                    className="border-[1px] border-black pl-3 rounded h-9"
                     type="text"
                     id="email"
                     required="email"
@@ -37,20 +37,20 @@ const Signup = () => {
                   />
                 </div>
                 <div className="flex  gap-8 w-full">
-                  <div className="flex flex-col">
+                  <div className="w-3/6 flex flex-col sm:w-2/4">
                     <label htmlFor="name">Name</label>
                     <input
-                      className="border-2 rounded h-9"
+                      className="border-[1px] border-black pl-3 rounded h-9"
                       type="text"
                       id="name"
                       required="text"
                       placeholder="Erik"
                     />
                   </div>
-                  <div className="flex flex-col">
+                  <div className="w-3/6 max-w-[50%] sm:w-2/4 flex flex-col">
                     <label htmlFor="lastname">Lastname</label>
                     <input
-                      className="border-2 rounded h-9"
+                      className="border-[1px] border-black pl-3 rounded h-9"
                       type="text"
                       id="lastname"
                       required="text"
@@ -58,10 +58,10 @@ const Signup = () => {
                     />
                   </div>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col gap-2">
                   <label htmlFor="password">Password</label>
                   <input
-                    className="border-2 rounded h-9"
+                    className="border-[1px] border-black pl-3 rounded h-9"
                     type="password"
                     id="password"
                     required="password"
@@ -69,8 +69,15 @@ const Signup = () => {
                 </div>
               </div>
 
-              <button type="submit">create Account</button>
-              <a href="#">Log in</a>
+              <button
+                className="m-6 mx-auto rounded-sm h-9 bg-blue-700 w-full"
+                type="submit"
+              >
+                CREATE ACOUNT
+              </button>
+              <a className="flex justify-center sm:mx-auto" href="#">
+                or Log in
+              </a>
             </form>
           </div>
         </div>
